bf90f193c38fd44d013888e334a46b924d5bf393
Don Svederus <svederus@gmail.com>
2022-05-23 22:49:57 -0600

    0.38.1 JS script to toggle, open and close

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+
+
 
